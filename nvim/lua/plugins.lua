457a4950b18c15af98f6c81546e9863af233850b
Joe Drew <joe@drew.ca>
2023-11-23 14:12:46 -0500

    Format typescript as well as javascript

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -70,7 +70,10 @@ return {
             formatters_by_ft = {
                 lua = { "stylua" },
                 python = { "black", "ruff_fix" },
-                javascript = { { "prettierd", "prettier" } },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
             },
             format_on_save = { timeout_ms = 500, lsp_fallback = true },
         },

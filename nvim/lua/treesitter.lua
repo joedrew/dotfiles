3f5cc07f52a819f32c3d59d27ca4382b423df89e
Joe Drew <joe@drew.ca>
2024-03-28 14:12:13 -0400

    Treesitter ruby

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -1,6 +1,18 @@
 require("nvim-treesitter.configs").setup({
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "typescript", "javascript", "swift", "kotlin" },
+    ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "python",
+        "typescript",
+        "javascript",
+        "swift",
+        "kotlin",
+        "ruby",
+    },
     auto_install = true,
     highlight = {
         enable = true,
